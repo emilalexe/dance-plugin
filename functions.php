1d00bc0f01b43c23438104f5df45bd4fbb8baff9
Emil Alexe <emil.alexe92@gmail.com>
2020-04-16 15:13:26 +0300

    Bootstrap + Fontawesome (Part VI)

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Dance Competition
+Plugin URI: https://www.devprolab.com/wordpress/plugin/dance-competition
+Description: Wordpress Plugin for Dance Competition
+Author: Emil ALEXE
+Version: 1.0
+Author URI: http://emil.alexe.xyz
+*/
